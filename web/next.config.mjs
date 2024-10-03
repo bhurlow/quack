f7238063dce9a50388d7e56f23666c5c448d0a66
Brian Hurlow <brian@brianhurlow.com>
2024-10-03 00:13:51 -0400

    use cdn based duckdb download

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,20 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: function (config, { isServer, dev }) {
-    config.output.webassemblyModuleFilename =
-      isServer && !dev
-        ? "..static/wasm/[name].[moduleHash].wasm"
-        : "static/wasm/[name].[moduleHash].wasm";
+    // config.output.webassemblyModuleFilename =
+    //   isServer && !dev
+    //     ? "..static/wasm/[name].[moduleHash].wasm"
+    //     : "static/wasm/[name].[moduleHash].wasm";
+
+    // config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    // config.output.assetModuleFilename = "static/[hash][ext]";
+
+    // config.module.rules.push({
+    //   test: /.*\.wasm$/,
+    //   type: "asset/resource",
+    //   // generator: {
+    //   //   filename: "static/wasm/[name].[contenthash][ext]",
+    //   // },
+    // });
+
+    // config.output.webassemblyModuleFilename =
+    //   isServer && !dev
+    //     ? "..static/wasm/[name].[moduleHash].wasm"
+    //     : "static/wasm/[name].[moduleHash].wasm";
 
     config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
 
     config.module.rules.push({
       test: /.*\.wasm$/,
       type: "asset/resource",
-      generator: {
-        filename: "static/wasm/[name].[contenthash][ext]",
-      },
     });
+
+    config.output.assetModuleFilename = "static/[hash][ext]";
 
     return config;
   },
--- web/src/lib/duckdb.ts
+++ web/src/lib/duckdb.ts
@@ -1,32 +1,47 @@
 import * as duckdb from "@duckdb/duckdb-wasm";
 
-// @ts-expect-error can't import
-import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm";
-// @ts-expect-error can't import
-import duckdb_wasm_next from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm";
+// const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
 
-const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
-  mvp: {
-    mainModule: duckdb_wasm,
-    mainWorker: new URL(
-      "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js",
-      import.meta.url
-    ).toString(),
-  },
-  eh: {
-    mainModule: duckdb_wasm_next,
-    mainWorker: new URL(
-      "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js",
-      import.meta.url
-    ).toString(),
-  },
-};
+// // @ts-expect-error can't import
+// import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm";
+// // @ts-expect-error can't import
+// import duckdb_wasm_next from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm";
+
+// const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+//   mvp: {
+//     mainModule: duckdb_wasm,
+//     mainWorker: new URL(
+//       "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js",
+//       import.meta.url
+//     ).toString(),
+//   },
+//   eh: {
+//     mainModule: duckdb_wasm_next,
+//     mainWorker: new URL(
+//       "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js",
+//       import.meta.url
+//     ).toString(),
+//   },
+// };
 
 export const initDb = async () => {
+  const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+
   // Select a bundle based on browser checks
-  const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+  const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+
+  const worker_url = URL.createObjectURL(
+    new Blob([`importScripts("${bundle.mainWorker!}");`], {
+      type: "text/javascript",
+    })
+  );
+
+  // Select a bundle based on browser checks
+  // const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
   // Instantiate the asynchronous version of DuckDB-Wasm
-  const worker = new Worker(bundle.mainWorker!);
+  // const worker = new Worker(bundle.mainWorker!);
+
+  const worker = new Worker(worker_url);
   const logger = new duckdb.ConsoleLogger();
   const db = new duckdb.AsyncDuckDB(logger, worker);
   await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
