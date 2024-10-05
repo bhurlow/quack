199307b8ca73d241c8b51550c86f70449ec0f2fd
Brian Hurlow <brian@brianhurlow.com>
2024-10-05 01:09:28 -0400

    fix

--- web/app/app/page.tsx
+++ web/app/app/page.tsx
@@ -3,7 +3,6 @@ import { Duck } from "@/src/components/duck";
 export default function Quack() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h3 className="text-blue-500 text-3xl">text-red-500 </h3>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Duck />
       </main>
